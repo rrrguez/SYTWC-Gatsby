074193caa4c8ee6ca5401ef92fabd3b1dcee4cf5
rrrguez <alu0101394763@ull.edu.es>
2024-12-28 13:15:46 -0800

    Añadido filtro

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -13,7 +13,15 @@ const Layout = ({children, section_title}) => {
                     <Link className='nav-link' to="../mario"> Mario </Link>
                     <Link className='nav-link' to="../zelda"> The Legend of Zelda </Link>
                 </nav>
-                <h2 className='section-title'>{section_title}</h2>
+                <div className='section-header'>
+                    <h2 className='section-title'>{section_title}</h2>
+                    <select className='sorter' id='news-sorter'>
+                        <option value="publishedAt"> Most recent </option>
+                        <option value="popularity"> Popularity </option>
+                        <option value="relevancy" selected> Relevancy </option>
+                    </select>
+
+                </div>
             </header>
             <div className='body' id='news-container'>{children}</div>
             <footer>© 2024 NINTOPIC - Rebeca R. R. </footer>
